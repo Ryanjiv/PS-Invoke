b7f413169394cf586a81ec2b9640fa5dd066dcdb
ryanjiv <ryanjiv@outlook.com>
2021-10-10 09:21:09 +0800

    add get-services.ps and edit psinvoke.ps1

--- PSinvoke.ps1
+++ PSinvoke.ps1
@@ -1,4 +1,4 @@
-Invoke-Command -ComputerName (Get-Content Servers.txt) -FilePath .\Sample.ps1 -ArgumentList Process, Service
+Invoke-Command -ComputerName (Get-Content Servers.txt) -FilePath .\get-service.ps1 -ArgumentList Process, Service
 
 
 ## https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/invoke-command?view=powershell-7.1
---
+++ get-service.ps1
@@ -0,0 +1,4 @@
+get-service
+
+
+## https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/invoke-command?view=powershell-7.1
